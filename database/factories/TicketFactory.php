1afc68d021414d5f513593620c94a3103b03f74f
pylypenko <yevhenii.pylypenko@gmail.com>
2024-12-18 22:01:59 +0000

    feat: :sparkles: create ticket model with migration, factory and seeder

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ticket>
+ */
+class TicketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'subject' => fake()->sentence(),
+            'content' => fake()->paragraph(),
+            'user_id' => User::factory(),
+            'status' => false,
+        ];
+    }
+}
--- database/migrations/2024_12_18_124153_create_tickets_table.php
+++ database/migrations/2024_12_18_124153_create_tickets_table.php
@@ -8,19 +8,18 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('SET NULL');
+            $table->string('subject');
+            $table->text('content');
+            $table->boolean('status')->default(false);
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };

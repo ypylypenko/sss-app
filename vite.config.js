4a9798231c7c34170572d909f95a932f72600b4e
pylypenko <yevhenii.pylypenko@gmail.com>
2024-12-19 16:57:38 +0000

    feat: :heavy_plus_sign: installed frontend dependencies, update vite config, configure docker for running frontend

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
+        react()
     ],
+    server: {
+        https: false,
+        host: true,
+        strictPort: true,
+        port: 5173,
+        hmr: {host: 'localhost', protocol: 'ws'},
+        watch: {
+            usePolling:true,
+        }
+    }
 });
